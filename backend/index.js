8521369f14cfea287d6c5b24c4bda865318aa634
Abiola Fadipe <abiolafadipe88@gmail.com>
2025-02-02 17:35:14 +0100

    file size download limit fix

--- backend/index.js
+++ backend/index.js
@@ -11,6 +11,9 @@ require("dotenv").config();
 
 const app = express();
 
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ limit: "1mb", extended: true }));
+
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
